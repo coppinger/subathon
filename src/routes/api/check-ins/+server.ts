b48d95ba3ccdc6055eef77e553ec4b371a477ae7
Charlie Coppinger <charles.coppinger@gmail.com>
2024-05-09 13:06:37 +1200

    get up endpoint

---
+++ src/routes/api/check-ins/+server.ts
@@ -0,0 +1,27 @@
+import { json, error } from '@sveltejs/kit';
+import type { RequestHandler } from './$types';
+
+import { PRIVATE_SECRET_TOKEN } from '$env/static/private';
+
+export const POST: RequestHandler = async ({ request }) => {
+	const token = request.headers.get('Authorization');
+
+	if (token !== `Pelion ${PRIVATE_SECRET_TOKEN}`) {
+		console.log('Unauthorized');
+		throw error(401, 'Unauthorized');
+	}
+
+	console.log('Authorized');
+
+	let data;
+
+	try {
+		data = await request.json();
+	} catch (err) {
+		error(400, 'Invalid JSON');
+	}
+
+	console.log(data.yeet);
+
+	return json({ data });
+};
