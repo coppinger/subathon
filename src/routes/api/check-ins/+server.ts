a61171b6ba1988e4aca0ece834695971c89f8ba6
Charlie Coppinger <charles.coppinger@gmail.com>
2024-05-09 13:13:53 +1200

    change secret token name

--- src/routes/api/check-ins/+server.ts
+++ src/routes/api/check-ins/+server.ts
@@ -1,12 +1,12 @@
 import { json, error } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
 
-import { PRIVATE_SECRET_TOKEN } from '$env/static/private';
+import { CHECKMATE_SECRET_TOKEN } from '$env/static/private';
 
 export const POST: RequestHandler = async ({ request }) => {
 	const token = request.headers.get('Authorization');
 
-	if (token !== `Pelion ${PRIVATE_SECRET_TOKEN}`) {
+	if (token !== `Pelion ${CHECKMATE_SECRET_TOKEN}`) {
 		console.log('Unauthorized');
 		throw error(401, 'Unauthorized');
 	}
@@ -20,8 +20,6 @@ export const POST: RequestHandler = async ({ request }) => {
 	} catch (err) {
 		error(400, 'Invalid JSON');
 	}
-
-	console.log(data.yeet);
 
 	return json({ data });
 };
